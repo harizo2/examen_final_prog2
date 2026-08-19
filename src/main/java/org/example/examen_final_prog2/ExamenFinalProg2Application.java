b8365ae8ef7910d72876adac7f02122294987ad3
Harizo <hei.harizo.2@gmail.com>
2026-08-19 21:54:40 +0300

    first commit

---
+++ src/main/java/org/example/examen_final_prog2/ExamenFinalProg2Application.java
@@ -0,0 +1,13 @@
+package org.example.examen_final_prog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalProg2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalProg2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examen_final_prog2/ExamenFinalProg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examen_final_prog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalProg2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
